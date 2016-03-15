c616f45aca2c001e9aa21f350b7f9a7cb580a223
Lokesh Parappurath <l_parapp@encs.concordia.ca>
2016-03-15 18:52:53 -0400

    cleaning code and repository

--- src/AllTests.java
+++ src/AllTests.java
@@ -3,7 +3,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ MapMakerTest.class, MapValidatorTest.class })
+@SuiteClasses({ MapMakerTest.class})
 public class AllTests {
 
 }
