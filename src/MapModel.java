b55812fcc8afc632357a03c51f7968bd2439fa47
Lokesh <lokesh1990@gmail.com>
2016-03-16 01:02:14 -0400

    fixed map validation bug

--- src/MapModel.java
+++ src/MapModel.java
@@ -134,7 +134,124 @@ class MapModel {
 		}
 		
 		if(isstart & isend & isatleastonepathexists & (!isorphanpathexist))
+		{
+			// check for a single path ---- breadth first search
+			String[] nodequ = new String[81];
+			int qstrt = 0, qend = 0;
+			int[][] distarr = new int[rsize][csize];
+			String[][] prevnode = new String[rsize][csize];
+			int k,i;
+			for(k=0;k<rsize;k++)
+			{
+				for(i=0;i<csize;i++)
+				{
+					distarr[k][i] = 999;
+					prevnode[k][i] = "";
+					if(maparray[k][i] == 1)
+					{
+						distarr[k][i] = 0;
+						prevnode[k][i] = "S";
+						nodequ[qend] = k + "" + i;
+						qend++;
+					}
+				}
+			}
+			char[] currele;
+			int maxdist = 0;
+			while(qstrt != qend)
+			{
+				currele = nodequ[qstrt].toCharArray();
+				qstrt++;
+				k = Integer.parseInt(String.valueOf(currele[0]));
+			    i = Integer.parseInt(String.valueOf(currele[1]));
+			    
+			    if(k < rsize - 1 && maparray[k + 1][i] > 1)
+				{
+			    	if(distarr[k + 1][i] == 999)
+			    	{
+			    		distarr[k + 1][i] = distarr[k][i] + 1;
+			    		prevnode[k + 1][i] = k + "" + i;
+			    		nodequ[qend] = (k + 1) + "" + i;
+						qend++;
+			    	}
+				}
+				if(k > 0 && maparray[k - 1][i] > 1)
+				{
+					if(distarr[k - 1][i] == 999)
+			    	{
+			    		distarr[k - 1][i] = distarr[k][i] + 1;
+			    		prevnode[k - 1][i] = k + "" + i;
+			    		nodequ[qend] = (k - 1) + "" + i;
+						qend++;
+			    	}
+				}
+				if(i < csize - 1 && maparray[k][i + 1] > 1)
+				{
+					if(distarr[k][i + 1] == 999)
+					{
+			    		distarr[k][i + 1] = distarr[k][i] + 1;
+			    		prevnode[k][i + 1] = k + "" + i;
+			    		nodequ[qend] = k + "" + (i + 1);
+						qend++;
+			    	}
+				}
+				if(i > 0 && maparray[k][i - 1] > 1)
+				{
+					if(distarr[k][i - 1] == 999)
+			    	{
+			    		distarr[k][i - 1] = distarr[k][i] + 1;
+			    		prevnode[k][i - 1] = k + "" + i;
+			    		nodequ[qend] = k + "" + (i - 1);
+						qend++;
+			    	}
+				}
+			    if(maxdist < distarr[k][i])
+			    	maxdist = distarr[k][i];
+			}
+			maxdist++;
+			currele = null;
+			for(k=0;k<rsize;k++)
+			{
+				for(i=0;i<csize;i++)
+				{
+					if(maparray[k][i] == 1)
+						maparrclone[k][i] = 1;
+					else if(maparray[k][i] == 9999)
+					{
+						maparrclone[k][i] = 9999;
+						currele = (k + "" + i).toCharArray();
+					}
+					else
+						maparrclone[k][i] = 0;
+				}
+			}
+			k = Integer.parseInt(String.valueOf(currele[0]));
+		    i = Integer.parseInt(String.valueOf(currele[1]));
+			
+		    while(maparrclone[k][i] != 1)
+		    {
+		    	distarr[k][i] = 999;
+		    	if(maparrclone[k][i] != 9999)
+		    		maparrclone[k][i] = maxdist;
+		    	
+		    	maxdist--;
+		    	currele = prevnode[k][i].toCharArray();
+		    	k = Integer.parseInt(String.valueOf(currele[0]));
+			    i = Integer.parseInt(String.valueOf(currele[1]));
+		    }
+		    distarr[k][i] = 999;
+		    for(k=0;k<rsize;k++)
+			{
+				for(i=0;i<csize;i++)
+				{
+					if(distarr[k][i] < 999)
+						return false;
+				}
+			}
+		    
+		    maparray = maparrclone;
 			return  true;
+		}
 		else
 			return false;
 		
---
+++ src/MapModelTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MapModelTest {
+
+	/**
+	 * Method to test if the 
+	 */
+	@Test
+	public void testnostartpoint() 
+	{
+		int[][] maparr =  new int[][]{ { 0, 0, 0, 0 },
+			{ 0, 2, 3, 0},
+			{ 0, 0, 4, 0},
+			{ 0, 0, 9999, 0}}; 
+		
+		MapModel testmapobj = new MapModel("testmap", maparr);
+		assertFalse(testmapobj.validateMap());
+		
+	}
+
+}
